7ca70d8529a12d47f758a8fbc8b9f6dc3b0c882b
LarisaChAng <LChS81@mail.ru>
2022-10-30 18:58:25 +0800

    Добавьте файлы проекта.

---
+++ Metod1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metod1
+{
+    class Program
+    {
+        static double GetSquare(double a, double b, double c)
+        {
+            double pp = (a + b + c) / 2;
+            double Ss = Math.Sqrt(pp * ((pp - a) * (pp) - b) * (pp - c));
+            return Ss;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите стороны первого треугольника");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            double z1 = Convert.ToDouble(Console.ReadLine());
+            double sq1 = GetSquare(x1, y1, z1);
+            
+            Console.WriteLine("Введите стороны второго треугольника");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            double z2 = Convert.ToDouble(Console.ReadLine());
+            double sq2 = GetSquare(x2, y2, z2);            
+
+            if (sq1 > sq2)
+                Console.WriteLine("Сравниваем {0:.00} и {1:.00}. Вывод - площадь первого треугольника больше", sq1, sq2);
+            else
+                if (sq1 < sq2)
+                Console.WriteLine("Сравниваем {0:.00} и {1:.00}. Вывод - площадь второго треугольника больше", sq1, sq2);
+            else
+                if (sq1 == sq2)
+                Console.WriteLine("Сравниваем {0:.00} и {1:.00}. Вывод - площади равны", sq1, sq2);
+            else                
+            Console.WriteLine("Некорректно введены стороны одного из треугольников: 1-го ({0:.00}) или 2-го ({1:.00})", sq1, sq2);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Metod2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metod2
+{
+    class Program
+    {
+        static void GetParamsCub(double a, out double s, out double v)
+        {
+            s = 6*Math.Pow(a,2);
+            v = Math.Pow(a, 3);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сторону квадрата (ребро)");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double s;
+            double v;
+            GetParamsCub(a, out s, out v);
+            Console.WriteLine("Площадь куба {0:.00}. Объем куба {1:.00}", s, v);
+            Console.ReadLine();
+          
+        }
+    }
+}
